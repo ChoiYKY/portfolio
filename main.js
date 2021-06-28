1f6f45365e35e2e8f086364a831d2200136be565
numeru <cyk000418@naver.com>
2021-06-29 04:03:40 +0900

    final touch

--- main.js
+++ main.js
@@ -16,16 +16,10 @@ if (window.performance) {
   scrollIntoView("#home");
 }
 
-// window.addEventListener("DOMContentLoaded", function () {
-//   pageCnt = 1;
-//   scrollIntoView("#home");
-// });
-
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
-  // console.log(`scroll : ${window.scrollY}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--move");
   } else {
@@ -59,37 +53,6 @@ function scrollIntoView(selection) {
   }
 }
 
-// const navbarMenu1 = document.querySelector(".navbar__menu1");
-// const navbarMenu2 = document.querySelector(".navbar__menu2");
-
-// navbarMenu1.addEventListener("click", (event) => {
-//   let prev;
-//   const target = event.target;
-//   const link = target.dataset.link;
-
-//   if (link == null) {
-//     return;
-//   }
-
-//   for (let i = 1; i < sections.length - 1; i++) {
-//     if (link == sections[i] && i > 1) {
-//       console.log(sections[i - 1]);
-//       prev = sections[i - 1];
-//     }
-//   }
-
-//   const targetSection = document.querySelector(link);
-//   scrollIntoView(link);
-//   setTimeout(() => {
-//     targetSection.classList.add("cur-page");
-//   }, 500);
-//   const prevSection = document.querySelector(prev);
-//   if (prevSection) {
-//     prevSection.classList.remove("cur-page");
-//   }
-//   navbarMenu2.classList.remove("open");
-// });
-
 // Navbar toggle button for small screen;
 
 const toggle_btn = document.querySelector(".navbar__toggle-btn");
@@ -105,19 +68,6 @@ contact_btn.addEventListener("click", () => {
   scrollIntoView("#contact");
   pageCnt = 5;
 });
-
-//Make home slowly fade to transparent as the window scrolls down;
-
-// const home = document.getElementById("home");
-// let homeHeight = home.getBoundingClientRect().height;
-// const homeElements = document.querySelectorAll(".home");
-// document.addEventListener("scroll", () => {
-//   for (let i = 0; i < homeElements.length; i++) {
-//     homeElements[i].style.opacity = 1 - window.scrollY / homeHeight;
-//   }
-// });
-
-// scroll "arrow down"
 
 const arrowDown = document.querySelector(".arrowDown_icon");
 
@@ -147,169 +97,6 @@ arrowUp.addEventListener("click", () => {
   }
   pageCnt--;
 });
-// document.addEventListener("scroll", () => {
-//   if (window.scrollY > homeHeight / 2) arrowUp.classList.add("visible");
-//   else arrowUp.classList.remove("visible");
-// });
-
-// const arrowUP = document.querySelector(".arrowUp_icon");
-// arrowUP.addEventListener("click", () => {
-//   scrollIntoView("#home");
-// });
-
-//Filter Projects
-
-const workBtnContainer = document.querySelector(".work__categories");
-const projectContainer = document.querySelector(".work__projects");
-const projects = document.querySelectorAll(".project");
-
-workBtnContainer.addEventListener("click", (e) => {
-  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if (filter == null) return;
-
-  const preSelected = workBtnContainer.querySelector(".active");
-  const curSelected =
-    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
-
-  preSelected.classList.remove("active");
-  curSelected.classList.add("active");
-
-  projectContainer.classList.add("anim-out");
-  setTimeout(() => {
-    projects.forEach((project) => {
-      if (filter === "*" || filter === project.dataset.type) {
-        project.classList.remove("invisible");
-      } else {
-        project.classList.add("invisible");
-      }
-    });
-    projectContainer.classList.remove("anim-out");
-  }, 300);
-});
-
-//Set border to selected Navbar menu;
-
-// const about = document.getElementById("about");
-// let aboutHeight = about.getBoundingClientRect().height;
-
-// const skills = document.getElementById("skills");
-// let skillsHeight = skills.getBoundingClientRect().height;
-
-// const work = document.getElementById("work");
-// let workHeight = work.getBoundingClientRect().height;
-
-// const testimomials = document.getElementById("testimonials");
-// let testimonialsHeight = testimomials.getBoundingClientRect().height;
-
-// const contact = document.getElementById("contact");
-// let contactHeight = contact.getBoundingClientRect().height;
-
-// let sectionScroll = [
-//   0,
-//   homeHeight - 30,
-//   aboutHeight - 30,
-//   skillsHeight - 30,
-//   workHeight - 30,
-//   testimonialsHeight - 30,
-//   contactHeight - 30,
-// ];
-// let scrollLimit =
-//   document.documentElement.scrollHeight - window.innerHeight - 10;
-
-// for (let i = 1; i < sectionScroll.length; i++) {
-//   sectionScroll[i] += sectionScroll[i - 1];
-// }
-
-// window.visualViewport.addEventListener("resize", () => {
-//   homeHeight = home.getBoundingClientRect().height;
-//   aboutHeight = about.getBoundingClientRect().height;
-//   skillsHeight = skills.getBoundingClientRect().height;
-//   workHeight = work.getBoundingClientRect().height;
-//   testimonialsHeight = testimomials.getBoundingClientRect().height;
-//   contactHeight = contact.getBoundingClientRect().height;
-
-//   sectionScroll = [
-//     0,
-//     homeHeight - 30,
-//     aboutHeight - 30,
-//     skillsHeight - 30,
-//     workHeight - 30,
-//     testimonialsHeight - 30,
-//     contactHeight - 30,
-//   ];
-//   scrollLimit = document.documentElement.scrollHeight - window.innerHeight - 30;
-
-//   for (let i = 1; i < sectionScroll.length; i++) {
-//     sectionScroll[i] += sectionScroll[i - 1];
-//   }
-// });
-
-// const navbarMenu = document.querySelectorAll(".navbar__menu__item");
-// document.addEventListener("scroll", () => {
-//   const preSection = document.querySelector(".selected");
-//   for (let i = 0; i < sectionScroll.length - 1; i++) {
-//     if (scrollLimit <= window.scrollY) {
-//       preSection.classList.remove("selected");
-//       navbarMenu[navbarMenu.length - 1].classList.add("selected");
-//       return;
-//     }
-//     if (
-//       sectionScroll[i] <= window.scrollY &&
-//       window.scrollY < sectionScroll[i + 1]
-//     ) {
-//       preSection.classList.remove("selected");
-//       navbarMenu[i].classList.add("selected");
-//       return;
-//     }
-//   }
-// });
-
-// window.visualViewport.addEventListener("resize", () => {
-//   console.log(`limit : ${scrollLimit}`);
-//   const preSection = document.querySelector(".selected");
-//   for (let i = 0; i < sectionScroll.length - 1; i++) {
-//     if (scrollLimit <= window.scrollY) {
-//       preSection.classList.remove("selected");
-//       navbarMenu[navbarMenu.length - 1].classList.add("selected");
-//       return;
-//     }
-//     if (
-//       sectionScroll[i] <= window.scrollY &&
-//       window.scrollY < sectionScroll[i + 1]
-//     ) {
-//       preSection.classList.remove("selected");
-//       navbarMenu[i].classList.add("selected");
-//       return;
-//     }
-//   }
-// });
-
-//****************
-
-// let prevPage = 0;
-
-// document.addEventListener("scroll", () => {
-//   if (screen.height == window.scrollY) {
-//     console.log(
-//       `screen.height: ${screen.height} and window.scrollY = ${window.scrollY} goodgood!!!`
-//     );
-//   }
-
-//   // curPage.classList.toggle("cur-page");
-//   for (let i = 5; i > 0; i--) {
-//     if (
-//       screen.height * i - 200 <= window.scrollY &&
-//       window.scrollY <= screen.height * (i + 1) - 200 &&
-//       prevPage !== i
-//     ) {
-//       const curPage = document.querySelector(sections[i]);
-//       console.log(curPage);
-//       curPage.classList.add("cur-page");
-//       prevPage = i;
-//       return;
-//     }
-//   }
-// });
 
 // image slider
 
@@ -334,8 +121,6 @@ function showDivs(n) {
   console.log(slideIndex);
   x[slideIndex].classList.add("visible");
 }
-
-//******************
 
 const nameCard = document.querySelector(".contact__name-card");
 
